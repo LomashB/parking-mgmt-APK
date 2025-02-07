e6d63fdc211d8e4cd737d732560de8ffcb350f85
Lomash Bhuva <lomashbhuva@gmail.com>
2025-02-07 11:13:38 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:parkingapk/screens/booking_screen.dart';
+import 'package:parkingapk/screens/ev_screen.dart';
+import 'package:parkingapk/screens/map_screen.dart';
+import 'package:parkingapk/screens/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Autospot',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "autoSpotSP",
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      routes: {
+        '/Myhome_page': (context) => MyHomePage(selectedIndex: 0),
+        '/map_screen': (context) => map_screen(),
+        '/booking_screen': (context) => booking_screen(),
+        '/ev_screen': (context) => ev_screen(),
+      },
+      // home: MyHomePage(selectedIndex: 0,),
+      home: splash_screen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  int selectedIndex = 0;
+  MyHomePage({super.key, required this.selectedIndex});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<Widget> _widgetOptions = [
+    map_screen(),
+    booking_screen(),
+    ev_screen()
+  ];
+
+  Future<void> _onItemTapped(int index) async {
+    setState(() {
+      widget.selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        title: Text("Autospot",style: TextStyle(color: Colors.white),),
+      ),
+        bottomNavigationBar: Container(
+          height: 70,
+          child: BottomNavigationBar(
+            backgroundColor: Color(0x24E6FFFFFF),
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.red,
+            // currentIndex: _currentIndex,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.map),
+                label: "Map",
+              ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.book_outlined),
+                  label: "Booking",
+                ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.ev_station_outlined),
+                label: "EV",
+              ),
+            ],
+            currentIndex: widget.selectedIndex,
+            onTap: _onItemTapped,
+          ),
+        ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _widgetOptions.elementAt(widget.selectedIndex),
+          ],
+        ),
+      )
+    );
+  }
+}
